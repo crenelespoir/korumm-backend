34d79b190b6f3c0fea515d0a615be7cae4707f78
Crénel Espoir <crenelespoiraissi@gmail.com>
2026-08-18 15:41:13 +0100

    fix: activer CORS pour le front-end local

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true});
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const port = process.env.PORT ?? 3001;
   await app.listen(port);
